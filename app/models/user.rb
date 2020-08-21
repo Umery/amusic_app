48b03e126b14bb8683bcc7de94824a281619acb0
Umery <riku.u.1030@gmail.com>
2020-08-21 20:25:39 +0900

    Userモデル作成

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+      
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ },
+                      uniqueness: { case_sensitive: false }
+      
+    validates :password, length: { minimum: 6 },
+                         format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'chats/index'
+  devise_for :users
   root to: "chats#index"
+  get 'chats/index'
 end

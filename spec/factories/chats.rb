15f49ec465ec36474ca2549941581505cb8244bc
Umery <riku.u.1030@gmail.com>
2020-08-24 15:14:19 +0900

    chatテーブルの単体テスト

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :chat do
-    
+    category_id   { Faker::Number.within(range: 2..13) }
+    title {Faker::Name.initials}
+    content { Faker::Lorem.sentence }
+    image {"images/Fake.png"}
+    association :user
   end
 end

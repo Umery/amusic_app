eb798a1413d81591da0523a1ddbd45520b329cc0
Umery <riku.u.1030@gmail.com>
2020-09-18 18:28:36 +0900

    マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
   end
 
@@ -13,5 +14,17 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      flash[:success] = "ユーザ登録情報更新"
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :instrument)
   end
 end
